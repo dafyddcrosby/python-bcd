e3e3b71e8bab2c1cad4828a58a7f90b73822c931
Dafydd Crosby <dtcrsby@gmail.com>
2012-06-17 08:36:31 -0600

    Making this a simple package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='bcd',
+      version='0.1',
+      description='Library to handle binary-coded decimal conversion'
+      author='Dafydd Crosby',
+      author_email='dtcrsby@gmail.com',
+      url='https://github.com/dafyddcrosby/python-bcd',
+      py_modules=['bcd'],
+      )
