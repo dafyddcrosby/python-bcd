5266af9018efdcba560b9c7b4fce516d6eec2725
Dafydd Crosby <dtcrsby@gmail.com>
2012-06-17 08:39:51 -0600

    missing comma

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='bcd',
       version='0.1',
-      description='Library to handle binary-coded decimal conversion'
+      description='Library to handle binary-coded decimal conversion',
       author='Dafydd Crosby',
       author_email='dtcrsby@gmail.com',
       url='https://github.com/dafyddcrosby/python-bcd',
